3bdf57bef1823e027895b9b4fe6b64cd45aedbbd
Markus Himmel <markus@lean-fro.org>
2024-11-04 11:41:37 +0100

    Bump to latest nightly

--- Orderedtree/Examples/InversionCount.lean
+++ Orderedtree/Examples/InversionCount.lean
@@ -13,10 +13,10 @@ def inversions (l : List Nat) : Nat := Id.run do
   for x in l do
     let insPt : Nat := m.insertionPoint x
     ans := ans + (m.size - insPt)
-    m := m.insert x ()
+    m := DOrderedTree.Raw.insert x () ⟨m, sorry⟩
   return ans
 
 def ofList (l : List (Nat × Nat)) : DOrderedTree.Raw Nat (fun _ => Nat) :=
-  l.foldl (init := .leaf) (fun l (a, b) => l.insert a b)
+  l.foldl (init := .leaf) (fun l (a, b) => DOrderedTree.Raw.insert a b ⟨l, sorry⟩)
 
--- #eval! inversions [4, 3, 1, 2]
+#eval! inversions [4, 3, 1, 2]
